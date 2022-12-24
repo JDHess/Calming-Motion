8a0fed0a5774afb6c14b43fba6e05213c16dbaa0
Julie Hess <jul>
2022-12-23 20:05:50 -0800

    Initial Commit

---
+++ Calming Motion Watch App/Calming_MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calming_MotionApp.swift
+//  Calming Motion Watch App
+//
+//  Created by Julie Hess on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Calming_Motion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calming Motion/Calming_MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calming_MotionApp.swift
+//  Calming Motion
+//
+//  Created by Julie Hess on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Calming_MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
